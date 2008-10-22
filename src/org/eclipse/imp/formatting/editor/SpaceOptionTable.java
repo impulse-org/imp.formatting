752ee8bbf10409e90ba2c0e5a60d06d7f3a55cee
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2008-10-22 11:29:51 +0000

    added "IBM corporation" to copyright statements

--- src/org/eclipse/imp/formatting/editor/SpaceOptionTable.java
+++ src/org/eclipse/imp/formatting/editor/SpaceOptionTable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2008 
+* Copyright (c) IBM Corporation 2008 
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v1.0
 * which accompanies this distribution, and is available at
@@ -25,7 +25,6 @@ import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.VerifyEvent;
 import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.graphics.Image;
