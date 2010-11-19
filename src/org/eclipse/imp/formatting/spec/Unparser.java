fb23230e3255d37cfda496343874df252d18ad45
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-11-19 17:34:01 +0000

    printToString() now sets up the Transformer to indent the generated XML properly.

--- src/org/eclipse/imp/formatting/spec/Unparser.java
+++ src/org/eclipse/imp/formatting/spec/Unparser.java
@@ -1,19 +1,18 @@
 /*******************************************************************************
-* Copyright (c) IBM Corporation 2008 
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Jurgen Vinju (jurgenv@cwi.nl) - initial API and implementation
-
-*******************************************************************************/
-
+ * Copyright (c) IBM Corporation 2008 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jurgen Vinju (jurgenv@cwi.nl) - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.imp.formatting.spec;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Iterator;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -31,113 +30,103 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-
 public class Unparser {
-	
-	private Document dom;
+    private Document dom;
 
-	public String unparse(Specification spec) {
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		
-		try {
-		DocumentBuilder db = dbf.newDocumentBuilder();
+    public String unparse(Specification spec) {
+        DocumentBuilderFactory dbf= DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db= dbf.newDocumentBuilder();
+            dom= db.newDocument();
+            Element root= dom.createElement("formatter");
+            addSpec(spec, root);
+            dom.appendChild(root);
+            return printToString(dom);
+        } catch (ParserConfigurationException pce) {
+            return null;
+        }
+    }
 
-		dom = db.newDocument();
-		Element root = dom.createElement("formatter");
-		addSpec(spec, root);
-		dom.appendChild(root);
-		return printToString(dom);
+    private String printToString(Document dom) {
+        try {
+            ByteArrayOutputStream output= new ByteArrayOutputStream();
+            TransformerFactory tf= TransformerFactory.newInstance();
 
-		}catch(ParserConfigurationException pce) {
-			return null;
-		}
-	}
-	
-	private String printToString(Document dom){
-		try
-		{
-			ByteArrayOutputStream output = new ByteArrayOutputStream();
-			
-			Transformer t = TransformerFactory.newInstance().newTransformer();
-            t.setOutputProperty(OutputKeys.INDENT, "yes");  
-            
-			t.transform(new DOMSource(dom), new StreamResult(output));
-			
-			String result = output.toString();
-			
-			output.close();
-			return result;
-		} catch(IOException ie) {
-		    ie.printStackTrace();
-		    return "";
-		} catch (TransformerConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TransformerFactoryConfigurationError e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TransformerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;
-	}
+            tf.setAttribute("indent-number", 4); // work around bug in Sun's JRE 5.0: http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6296446
 
-	private void addSpec(Specification spec, Element root) {
-		Element language = dom.createElement("language");
-		language.setTextContent(spec.getLanguage());
-		
-		Element rules = dom.createElement("rules");
-		addRules(spec.ruleIterator(), rules);
-		
-		Element options = dom.createElement("space-options");
-		addOptions(spec, options);
-		
-		Element example = dom.createElement("example");
-		example.setTextContent(spec.getExample());
-		
-		root.appendChild(language);
-		root.appendChild(rules);
-		root.appendChild(options);
-		root.appendChild(example);
-	}
+            Transformer t= tf.newTransformer();
 
-	private void addRules(Iterator<Item> list, Element rules) {
-		while (list.hasNext()) {
-			Item item = list.next();
+            t.setOutputProperty(OutputKeys.INDENT, "yes");
+            t.setOutputProperty("{http://xml.apache.org/xalan}indent-amount", "4");
+            t.transform(new DOMSource(dom), new StreamResult(new OutputStreamWriter(output, "utf-8")));
 
-			if (item instanceof Rule) {
-				Rule rule = (Rule) item;
-				Element elem = dom.createElement("rule");
-				Element box = dom.createElement("box");
-				elem.appendChild(box);
-				box.setTextContent(rule.getBoxString());
-				Element preview = dom.createElement("preview");
-				elem.appendChild(preview);
-				preview.setTextContent(rule.getPatternString());
-				rules.appendChild(elem);
-			}
-			else if (item instanceof Separator) {
-				Separator sep = (Separator) item;
-				Element elem = dom.createElement("separator");
-				String label = sep.getLabel();
-				elem.setTextContent(label.length() > 0 ? label : "anonymous");
-				rules.appendChild(elem);
-			}
-		}
-	}
-	
-	private void addOptions(Specification spec, Element options) {
-		Iterator<String> names = spec.getSpaceOptions();
-		
-		while (names.hasNext()) {
-			String name = names.next();
-			Integer value = spec.getSpaceOption(name);
-			Element elem = dom.createElement("space-option");
-		    elem.setAttribute("name", name);
-		    elem.setAttribute("value", value.toString());
-		    options.appendChild(elem);
-		}
-	}
+            String result= output.toString();
+
+            output.close();
+            return result;
+        } catch (IOException ie) {
+            ie.printStackTrace();
+            return "";
+        } catch (TransformerConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TransformerFactoryConfigurationError e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void addSpec(Specification spec, Element root) {
+        Element language= dom.createElement("language");
+        language.setTextContent(spec.getLanguage());
+        Element rules= dom.createElement("rules");
+        addRules(spec.ruleIterator(), rules);
+        Element options= dom.createElement("space-options");
+        addOptions(spec, options);
+        Element example= dom.createElement("example");
+        example.setTextContent(spec.getExample());
+        root.appendChild(language);
+        root.appendChild(rules);
+        root.appendChild(options);
+        root.appendChild(example);
+    }
+
+    private void addRules(Iterator<Item> list, Element rules) {
+        while (list.hasNext()) {
+            Item item= list.next();
+            if (item instanceof Rule) {
+                Rule rule= (Rule) item;
+                Element elem= dom.createElement("rule");
+                Element box= dom.createElement("box");
+                elem.appendChild(box);
+                box.setTextContent(rule.getBoxString());
+                Element preview= dom.createElement("preview");
+                elem.appendChild(preview);
+                preview.setTextContent(rule.getPatternString());
+                rules.appendChild(elem);
+            } else if (item instanceof Separator) {
+                Separator sep= (Separator) item;
+                Element elem= dom.createElement("separator");
+                String label= sep.getLabel();
+                elem.setTextContent(label.length() > 0 ? label : "anonymous");
+                rules.appendChild(elem);
+            }
+        }
+    }
+
+    private void addOptions(Specification spec, Element options) {
+        Iterator<String> names= spec.getSpaceOptions();
+        while (names.hasNext()) {
+            String name= names.next();
+            Integer value= spec.getSpaceOption(name);
+            Element elem= dom.createElement("space-option");
+            elem.setAttribute("name", name);
+            elem.setAttribute("value", value.toString());
+            options.appendChild(elem);
+        }
+    }
 }
