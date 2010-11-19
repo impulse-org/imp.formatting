70ef608649300e86e79d938c1f3216ec59256ab3
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-11-19 17:32:06 +0000

    Added null guard to setLanguage(), in case the object Language isn't registered. setLanguage() now does something useful when an exception is encountered. parseObject() now emits a message to the message handler when there is no object language parser. Added conventional prefix to field names.

--- src/org/eclipse/imp/formatting/spec/Parser.java
+++ src/org/eclipse/imp/formatting/spec/Parser.java
@@ -7,7 +7,6 @@
 *
 * Contributors:
 *    Jurgen Vinju (jurgenv@cwi.nl) - initial API and implementation
-
 *******************************************************************************/
 
 package org.eclipse.imp.formatting.spec;
@@ -33,6 +32,10 @@ import org.eclipse.imp.model.ISourceProject;
 import org.eclipse.imp.model.ModelFactory.ModelException;
 import org.eclipse.imp.parser.IMessageHandler;
 import org.eclipse.imp.parser.IParseController;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -47,54 +50,50 @@ import org.xml.sax.helpers.DefaultHandler;
  * language, another parser is called to parse those. Finally, the example
  * source code is parsed with that same parser.
  * 
- * The parser constructs a
- * 
- * @see Specification object that contains all relevant information.
- * 
+ * The parser constructs a {@link Specification} object that contains all relevant information.
  * @author jurgenv
- * 
  */
 public class Parser extends DefaultHandler {
 
 	static protected SAXParserFactory spf = SAXParserFactory.newInstance();
 
-	protected Specification spec;
+	protected Specification fSpec;
 
-	protected Rule tmpRule;
+	protected Rule fTmpRule;
 
-	protected String tmpContents;
+	protected String fTmpContents;
 
-	private ISourceProject project;
+	private ISourceProject fProject;
 
-	private IPath path;
+	private IPath fPath;
 
-	private IParseController objectParser;
+    private Language fObjectLanguage;
 
-	private Language objectLanguage;
+	private IParseController fObjectParser;
 
-	private IParseController boxParser;
+	private IParseController fBoxParser;
 
-	private IProgressMonitor boxParserMonitor;
+	private IProgressMonitor fBoxParserMonitor;
 
-	private IProgressMonitor objectParserMonitor;
+	private IProgressMonitor fObjectParserMonitor;
 
-	private IMessageHandler handler;
+	private IMessageHandler fHandler;
 
-	private Separator tmpSeparator;
+	private Separator fTmpSeparator;
 
-	private boolean noObjectParsing;
+	private boolean fNoObjectParsing;
 
 	public Parser(IPath path, ISourceProject project, IMessageHandler handler) throws ModelException {
-		this.path = path;
-		this.spec = new Specification(this);
-		this.project = project;
-		this.handler = handler;
-		this.noObjectParsing = false;
+		this.fPath = path;
+		this.fSpec = new Specification(this);
+		this.fProject = project;
+		this.fHandler = handler;
+		this.fNoObjectParsing = false;
 	}
-	
-	public void setMessageHandler(IMessageHandler handler) {
-		this.handler = handler;
-	}
+
+	public IMessageHandler getMessageHandler() {
+	    return fHandler;
+    }
 
 	/**
 	 * Use this method to parse a specification file of which you have a path
@@ -104,16 +103,15 @@ public class Parser extends DefaultHandler {
 	 * @throws FileNotFoundException
 	 * @throws Exception
 	 */
-	public Specification parse(IPath path) throws ParseException,
-			FileNotFoundException {
-		this.path = path;
-		noObjectParsing = false;
+	public Specification parse(IPath path) throws ParseException, FileNotFoundException {
+		this.fPath = path;
+		fNoObjectParsing = false;
 		return parse(new InputSource(new FileReader(path.toOSString())));
 	}
 
 	public Specification parse(String inputString) throws ParseException {
 		// TODO: throw some sensible exception
-		noObjectParsing = false;
+		fNoObjectParsing = false;
 		return parse(new InputSource(new StringReader(inputString)));
 	}
 	
@@ -126,7 +124,7 @@ public class Parser extends DefaultHandler {
 	 * @throws ParseException
 	 */
 	public Specification load(String inputString) throws ParseException {
-		noObjectParsing = true;
+		fNoObjectParsing = true;
 		return parse(new InputSource(new StringReader(inputString)));
 	}
 
@@ -135,97 +133,105 @@ public class Parser extends DefaultHandler {
 			SAXParser sp = spf.newSAXParser();
 			sp.parse(input, this);
 
-			if (spec == null) {
-				throw new ParseException("Parsing of " + input
-						+ "failed miserably");
+			if (fSpec == null) {
+				throw new ParseException("Parsing of " + input + "failed.");
 			}
 
-			return spec;
+			return fSpec;
 		} catch (SAXException se) {
-			throw new ParseException("Parsing of " + input
-					+ " failed because XML was invalid", se);
+			throw new ParseException("Parsing of " + input + " failed because XML was invalid", se);
 		} catch (ParserConfigurationException pce) {
-			throw new ParseException("Parsing of " + input
-					+ " failed because XML configuration is wrong", pce);
+			throw new ParseException("Parsing of " + input + " failed because XML configuration is wrong", pce);
 		} catch (IOException ie) {
-			throw new ParseException("Parsing of " + input
-					+ " failed because of an input error", ie);
+			throw new ParseException("Parsing of " + input + " failed because of an input error", ie);
 		}
 	}
 
-	public void startElement(String uri, String localName, String qName,
-			Attributes attributes) throws SAXException {
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		if (qName.equals("rule")) {
-			tmpRule = new Rule();
+			fTmpRule = new Rule();
 		} else if (qName.equals("box")) {
-			tmpContents = "";
+			fTmpContents = "";
 		}
 	    else if (qName.equals("preview")) {
-			tmpContents = "";
+			fTmpContents = "";
 		} else if (qName.equals("example")) {
-			tmpContents = "";
+			fTmpContents = "";
 		} else if (qName.equals("language")) {
-			tmpContents = "";
+			fTmpContents = "";
 		} else if (qName.equals("separator")) {
-			tmpSeparator = new Separator();
-			tmpContents = "";
+			fTmpSeparator = new Separator();
+			fTmpContents = "";
 		} else if (qName.equals("space-option")) {
 			String name = attributes.getValue("name");
 			Integer value = Integer.parseInt(attributes.getValue("value"));
-			spec.setSpaceOption(name, value);
+			fSpec.setSpaceOption(name, value);
 		}
 	}
 
-	public void endElement(String uri, String localName, String qName)
-			throws SAXException {
+	public void endElement(String uri, String localName, String qName) throws SAXException {
 		if (qName.equals("rule")) {
-			spec.addRule(tmpRule);
+			fSpec.addRule(fTmpRule);
 		} else if (qName.equals("box")) {
-			if (tmpRule != null) {
-				if (noObjectParsing) {
-				  tmpRule.setBoxString(tmpContents);
-				}
-				else {
+			if (fTmpRule != null) {
+				if (fNoObjectParsing) {
+				    fTmpRule.setBoxString(fTmpContents);
+				} else {
 				  try {
-					parseBoxAndObject(tmpContents, tmpRule);
+				      parseBoxAndObject(fTmpContents, fTmpRule);
 				  } catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				      e.printStackTrace();
 				  }
 				}
 			}
-		}
-		else if (qName.equals("preview")) {
-			if (tmpRule != null) {
-				tmpRule.setPatternString(tmpContents);
+		} else if (qName.equals("preview")) {
+			if (fTmpRule != null) {
+				fTmpRule.setPatternString(fTmpContents);
 			}
 		} else if (qName.equals("example")) {
-			spec.setExample(tmpContents);
-			spec.setExampleAst(parseObject(tmpContents));
+			fSpec.setExample(fTmpContents);
+			fSpec.setExampleAst(parseObject(fTmpContents));
 		} else if (qName.equals("language")) {
-			spec.setLanguage(tmpContents);
-			setLanguage(tmpContents);
+			fSpec.setLanguage(fTmpContents);
+			setLanguage(fTmpContents);
 		} else if (qName.equals("separator")) {
-			tmpSeparator.setLabel(tmpContents);
-			spec.addSeparator(tmpSeparator);
+			fTmpSeparator.setLabel(fTmpContents);
+			fSpec.addSeparator(fTmpSeparator);
 		} 
 	}
 
 	public void setLanguage(String langName) {
-		if (objectParser == null) {
+		if (fObjectParser == null) {
 			try {
-				objectLanguage = LanguageRegistry.findLanguage(langName);
-				ExtensionPointBinder b = new ExtensionPointBinder(objectLanguage);
-				objectParser = b.getObjectParser();
-			} catch (Exception e) {
-				// TODO do something sensible here
-				e.printStackTrace();
+				fObjectLanguage = LanguageRegistry.findLanguage(langName);
+				if (fObjectLanguage != null) {
+				    ExtensionPointBinder b = new ExtensionPointBinder(fObjectLanguage);
+
+				    fObjectParser = b.getObjectParser();
+				} else {
+				    displayAlert("Unknown target language", "A language descriptor for the language '" + langName + "' has not been registered.");
+				}
+			} catch (final Exception e) {
+			    String msg= e.getMessage().contains("astAdapter") ?
+			          "Unable to find specified AST adapter class; check the formatting extension in the plugin.xml" :
+			          "Unable to find target language parser class; check the formatting extension in the plugin.xml";
+
+			    displayAlert("Error initializing target language pattern parser", msg);
 			}
 		}
 	}
 
-	public void parseBoxAndObject(String boxString, Rule rule)
-			throws ParseException {
+	private void displayAlert(final String title, final String msg) {
+        Display display= PlatformUI.getWorkbench().getDisplay();
+        final Shell shell= display.getActiveShell();
+        display.asyncExec(new Runnable() {
+            public void run() {
+                MessageDialog.openError(shell, title, msg);
+            }
+        });
+	}
+
+	public void parseBoxAndObject(String boxString, Rule rule) throws ParseException {
 		rule.setBoxString(boxString);
 		rule.setBoxAst(parseBox(boxString));
 
@@ -235,33 +241,38 @@ public class Parser extends DefaultHandler {
 		}
 	}
 
-	public void characters(char[] ch, int start, int length)
-			throws SAXException {
-		tmpContents += new String(ch, start, length);
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		fTmpContents += new String(ch, start, length);
 	}
 
 	public IBox parseBox(String boxString) {
-		initializeBoxParser(); // TODO RMF This probably isn't necessary...
-		return (IBox) boxParser.parse(boxString, boxParserMonitor);
+		initializeBoxParser();
+		return (IBox) fBoxParser.parse(boxString, fBoxParserMonitor);
 	}
 
 	private void initializeBoxParser() {
-		if (boxParser == null) {
-			boxParser = new BoxParseController();
-			boxParserMonitor = new NullProgressMonitor();
+		if (fBoxParser == null) {
+			fBoxParser = new BoxParseController();
+			fBoxParserMonitor = new NullProgressMonitor();
 		}
-		boxParser.initialize(path, project, handler);
+		fBoxParser.initialize(fPath, fProject, fHandler);
 	}
 
 	public Object parseObject(String objectString) {
-		initializeObjectParser(objectParser);
-		return objectParser.parse(objectString, objectParserMonitor);
+	    if (fObjectParser == null) {
+	        fHandler.clearMessages();
+	        fHandler.handleSimpleMessage("Unable to parse formatted text: no parser is configured", 0, 0, 0, 0, 0, 0);
+	        fHandler.endMessages();
+	        return null;
+	    }
+		initializeObjectParser(fObjectParser);
+		return fObjectParser.parse(objectString, fObjectParserMonitor);
 	}
 
 	private void initializeObjectParser(IParseController parseController) {
-		if (objectParserMonitor == null) {
-			objectParserMonitor = new NullProgressMonitor();
+		if (fObjectParserMonitor == null) {
+			fObjectParserMonitor = new NullProgressMonitor();
 		}
-		parseController.initialize(path, project, handler);
+		parseController.initialize(fPath, fProject, fHandler);
 	}
 }
